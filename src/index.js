c490211aca5fbd146dfba1a2a0d2d23d28433ae6
Jean <chakib3500@gmail.com>
2022-05-17 16:54:32 +0200

    ajout d'un plugin babel pour les erreur react

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import "./scss/globals.scss";
 
 import React from "react";
 import ReactDOM  from "react-dom";
+
+function App() {
+  return (
+    <h1>hello react</h1>
+  )
+}
+
+ReactDOM.render(<App/>,document.querySelector(".root"))
